e75ca307d6c0a4cfea87e0f5108f52d3d5e685d8
flagship <flagship@abtasty.com>
2023-03-01 17:50:10 +0100

    Brew formula update for codebase-analyzer version v0.7.0

--- codebase-analyzer.rb
+++ codebase-analyzer.rb
@@ -5,11 +5,11 @@
 class CodebaseAnalyzer < Formula
   desc ""
   homepage "https://github.com/flagship-io/codebase-analyzer"
-  version "0.6.1"
+  version "0.7.0"
 
   on_macos do
-    url "https://github.com/flagship-io/codebase-analyzer/releases/download/v0.6.1/codebase-analyzer_0.6.1_darwin_all.tar.gz"
-    sha256 "42f7428c61ed524182b96795cd915fac54ac2ce27debf784723341805f593cf9"
+    url "https://github.com/flagship-io/codebase-analyzer/releases/download/v0.7.0/codebase-analyzer_0.7.0_darwin_all.tar.gz"
+    sha256 "427e4f888fb0aac820b56c0d646daa32d45d62ccd29d4ae3a3952517a665132e"
 
     def install
       bin.install "codebase-analyzer"
@@ -17,17 +17,17 @@ class CodebaseAnalyzer < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/flagship-io/codebase-analyzer/releases/download/v0.6.1/codebase-analyzer_0.6.1_linux_amd64.tar.gz"
-      sha256 "60bdf0fd190271d5afde2493e54d4fa07ebd9c5ad67a88cd9448947aa7b0d5b0"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/flagship-io/codebase-analyzer/releases/download/v0.7.0/codebase-analyzer_0.7.0_linux_arm64.tar.gz"
+      sha256 "f86b8b1736466942ba02b1e985d3d6c049a9ce6b8627a7faf6e35f0f39506bc3"
 
       def install
         bin.install "codebase-analyzer"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/flagship-io/codebase-analyzer/releases/download/v0.6.1/codebase-analyzer_0.6.1_linux_arm64.tar.gz"
-      sha256 "5eeacf3c5a4d4da4a939c57159e1a399897409b6798a9a627f7da427ec474c63"
+    if Hardware::CPU.intel?
+      url "https://github.com/flagship-io/codebase-analyzer/releases/download/v0.7.0/codebase-analyzer_0.7.0_linux_amd64.tar.gz"
+      sha256 "6ca4087fe1e3e2e249b8dc1b5d1d5182ee6ee17344b9bf1aee7f5dabaa12e3d0"
 
       def install
         bin.install "codebase-analyzer"
